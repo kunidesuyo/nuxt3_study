197046db15d7a8903f6ea9379d5ef92442733183
kuni <kunidesuyo1234@gmail.com>
2024-03-12 07:01:07 +0900

    3.1

--- routing-basic/app.vue
+++ routing-basic/app.vue
@@ -25,5 +25,23 @@ main {
   border: blue 1px solid;
   padding: 10px;
 }
-
+#breadcrumbs ul li {
+  display: inline;
+  list-style-type: none;
+}
+#breadcrumbs {
+  margin-left: 0px;
+}
+#breadcrumbs ul {
+  padding-left: 0px;
+}
+#breadcrumbs ul .current {
+  color: red;
+}
+#breadcrumbs ul li:before {
+  content: " > ";
+}
+#breadcrumbs ul li:first-child:before {
+  content: none;
+}
 </style>

ee13c9f2efdc0ab4ddb3a440e0ecc1eb236ad9f3
kuni <kunidesuyo1234@gmail.com>
2024-03-14 22:30:51 +0900

    4.1

---
+++ fetch/pages/WeatherInfo/[id].vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import type {City} from "@/interfaces";
+
+const route = useRoute();
+const cityList = useState<Map<number, City>>("cityList");
+const selectedCity = computed(
+  (): City => {
+    const idNo = Number(route.params.id);
+    return cityList.value.get(idNo) as City;
+  }
+);
+const weatherDescription = ref("");
+
+const weatherInfoUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+const params: {
+  lang: string;
+  q: string;
+  appid: string;
+} = 
+{
+  lang: "ja",
+  q: selectedCity.value.q,
+  appid: "11a7538fc51e3de1fea32319f0af5665"
+}
+const queryParams = new URLSearchParams(params);
+const urlFull = `${weatherInfoUrl}?${queryParams}`;
+const response = await $fetch(urlFull) as any;
+const weatherArray = response.weather;
+const weather = weatherArray[0];
+weatherDescription.value = weather.description;
+</script>
+
+<template>
+  <section>
+    <h2>{{ selectedCity.name }}の天気</h2>
+    <p>{{ weatherDescription }}</p>
+  </section>
+  <p>リストに<NuxtLink v-bind:to="{name: 'index'}">戻る</NuxtLink></p>
+</template>

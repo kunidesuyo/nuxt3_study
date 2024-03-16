3c0b7feee1886581442e94cf9c66b035ba533dc7
kuni <kunidesuyo1234@gmail.com>
2024-03-16 15:41:11 +0900

    4.4

--- fetch/pages/WeatherInfo/[id].vue
+++ fetch/pages/WeatherInfo/[id].vue
@@ -4,42 +4,49 @@ import type {City} from "@/interfaces";
 const route = useRoute();
 const cityList = useState<Map<number, City>>("cityList");
 const selectedCity = computed(
-  (): City => {
-    const idNo = Number(route.params.id);
-    return cityList.value.get(idNo) as City;
-  }
+	(): City => {
+		const idNo = Number(route.params.id);
+		return cityList.value.get(idNo) as City;
+	}
 );
-// const weatherDescription = ref("");
-const params: {
-  lang: string;
-  q: string;
-  appid: string;
-} = 
-{
-  lang: "ja",
-  q: selectedCity.value.q,
-  appid: "11a7538fc51e3de1fea32319f0af5665"
-}
 
-const asyncData = await useFetch(
-  "https://api.openweathermap.org/data/2.5/weather",
-  {
-    key: `/WeatherInfo/${route.params.id}`,
-    query: params,
-    transform: (data: any): string => {
-      const weatherArray = data.weather;
-      const weather = weatherArray[0];
-      return weather.description;
-    }
-  }
+const asyncData = useLazyAsyncData(
+	`/WeatherInfo/${route.params.id}`,
+	(): Promise<any> => {
+		const weatherInfoUrl = "https://api.openweathermap.org/data/2.5/weather";
+		const params:{
+			lang: string;
+			q: string;
+			appid: string;
+		} =
+		{
+			lang: "ja",
+			q: selectedCity.value.q,
+			//APIキーのクエリパラメータ。ここに各自の文字列を記述する!!
+			appid: "xxxxxx"
+		}
+		const queryParams = new URLSearchParams(params);
+		const urlFull = `${weatherInfoUrl}?${queryParams}`;
+		const response = $fetch(urlFull);
+		return response;
+	},
+	{
+		transform: (data): string => {
+			const weatherArray = data.weather;
+			const weather = weatherArray[0];
+			return weather.description;
+		}
+	}
 );
 const weatherDescription = asyncData.data;
+const pending = asyncData.pending;
 </script>
 
 <template>
-  <section>
-    <h2>{{ selectedCity.name }}の天気</h2>
-    <p>{{ weatherDescription }}</p>
-  </section>
-  <p>リストに<NuxtLink v-bind:to="{name: 'index'}">戻る</NuxtLink></p>
+	<p v-if="pending">データ取得中…</p>
+	<section v-else>
+		<h2>{{selectedCity.name}}の天気</h2>
+		<p>{{weatherDescription}}</p>
+	</section>
+	<p>リストに<NuxtLink v-bind:to="{name: 'index'}">戻る</NuxtLink></p>
 </template>

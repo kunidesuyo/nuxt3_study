79de0d44420ae8e4804eb03f171f34d8bbc76895
kuni <kunidesuyo1234@gmail.com>
2024-03-20 22:58:10 +0900

    chapter5

---
+++ server-storage/server/routes/member-management/members/[id].get.ts
@@ -0,0 +1,20 @@
+import type { Member, ReturnJSONMembers } from "~/interfaces";
+// import { createMemberList } from "~/memberDB";
+
+export default defineEventHandler(
+  async (event): Promise<ReturnJSONMembers> => {
+    const params = event.context.params;
+    let memberList = new Map<number, Member>();
+    const storage = useStorage();
+    const memberListStorage = await storage.getItem("local:member-management_members");
+    if(memberListStorage != undefined) {
+      memberList = new Map<number, Member>(memberListStorage as any);
+    }
+    const idNo = Number(params!.id);
+    const member = memberList.get(idNo) as Member;
+    return {
+      result: 1,
+      data: [member]
+    };
+  }
+);

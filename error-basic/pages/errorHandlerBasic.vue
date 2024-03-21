203f136cd67cd7c76814f08de690704ba06262fb
kuni <kunidesuyo1234@gmail.com>
2024-03-22 02:22:06 +0900

    6.1.3

--- error-basic/pages/errorHandlerBasic.vue
+++ error-basic/pages/errorHandlerBasic.vue
@@ -1,5 +1,11 @@
 <template>
-  <ErrorGeneratorBasic/>
+  <NuxtErrorBoundary>
+    <ErrorGeneratorBasic/>
+    <template v-slot:error="errorProps">
+      <p>エラーが発生しました！</p>
+      <p>{{ errorProps.error }}</p>
+    </template>
+  </NuxtErrorBoundary>
   <p>
     <NuxtLink v-bind:to="{name: 'index'}">
       戻る

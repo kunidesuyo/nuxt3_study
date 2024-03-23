0bd266fd7e85f12efb933b3d4b4e190def3f5616
kuni <kunidesuyo1234@gmail.com>
2024-03-23 22:48:43 +0900

    6.2

--- error-basic/pages/errorHandlerBasic.vue
+++ error-basic/pages/errorHandlerBasic.vue
@@ -1,9 +1,16 @@
+<script setup lang="ts">
+const onResetButtonClick = (error: Ref) => {
+  error.value = null;
+}
+</script>
+
 <template>
   <NuxtErrorBoundary>
     <ErrorGeneratorBasic/>
     <template v-slot:error="errorProps">
       <p>エラーが発生しました！</p>
       <p>{{ errorProps.error }}</p>
+      <button v-on:click="onResetButtonClick(errorProps.error)">エラーを解消</button>
     </template>
   </NuxtErrorBoundary>
   <p>
